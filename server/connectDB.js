e331ea4850929f1c93f95640e7a09fd0898b6a2d
Forrest Lin <forrest.lin.developer@gmail.com>
2023-05-05 18:38:57 +1000

    server init

---
+++ server/connectDB.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+// Connect to MongoDB and Mongoose Setup
+export const connectDB = (url) => {
+  mongoose.set("strictQuery", true);
+
+  mongoose
+    .connect(url)
+    .then(() => console.log("MongoDB connected..."))
+    .catch((error) => console.log(error));
+};
---
+++ server/middleware/auth.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+
+// verify the token
+export const verifyToken = async (req, res, next) => {
+  try {
+    let token = req.header('Authorization');
+
+    // check if token exists
+    if (!token) {
+      return res.status(403).send('Access Denied');
+    }
+
+    // remove the 'Bearer ' part of the token, this is setup at frontend
+    if (token.startsWith('Bearer ')) {
+      token = token.slice(7, token.length).trimLeft();
+    }
+
+    // verify the token using our secret string
+    const verified = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = verified;
+
+    // proceed to the next step of the function
+    next();
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
